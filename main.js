3042b2d1437e609c295fe9cfcf9bf3bf206a43f8
matheus-nascimento13 <mt.pereira@hotmail.com>
2023-07-25 23:23:24 -0300

    commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const listOfDestinations = new Array(
+    `Edinburgh`,
+    `New York`,
+    `London`,
+    `Oslo`,
+);
+
+const buyerAge = 18;
+const accompanied = false;
+const destiny = "Brazil";
+
+const canBuy = buyerAge >= 18 || accompanied == true;
+let destinyExist = false;
+
+let counter = 0;
+
+while (counter < 4) {
+    if (listOfDestinations[counter] == destiny) {
+        console.log("Great! We have tickets to your destiny!");
+        console.log("So, let's buy your Tickets!");
+        destinyExist = true;
+        break;
+    }
+    counter += 1;
+}
+
+if (canBuy == true && destinyExist == true) {
+    console.log("Choose the day of the ticket to", destiny);
+} else {
+    console.log("unfortunately you do not have the attributes to continue your purchase.");
+}
+
+
